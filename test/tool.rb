d338f9bcf76a42dc3acffe862f7ab4b1e47b9e24
Sebastián Katzer <katzer.sebastian@googlemail.com>
2017-08-08 10:19:36 +0200

    Added tool class

---
+++ test/tool.rb
@@ -0,0 +1,93 @@
+#
+# Copyright (c) 2016 by appPlant GmbH. All rights reserved.
+#
+# @APPPLANT_LICENSE_HEADER_START@
+#
+# This file contains Original Code and/or Modifications of Original Code
+# as defined in and that are subject to the Apache License
+# Version 2.0 (the 'License'). You may not use this file except in
+# compliance with the License. Please obtain a copy of the License at
+# http://opensource.org/licenses/Apache-2.0/ and read it before using this
+# file.
+#
+# The Original Code and all software distributed under the License are
+# distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+# EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+# INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+# Please see the License for the specific language governing rights and
+# limitations under the License.
+#
+# @APPPLANT_LICENSE_HEADER_END@
+
+class TestTool1 < ISS::Tool
+  self.bin = 'testtool1'
+end
+
+class TestTool2 < ISS::Tool
+  self.bin = 'testtool2'
+  self.path = '/path/to'
+end
+
+class TestTool3 < ISS::Tool
+  self.bin  = 'testtool3'
+  self.path = '/path/to'
+  self.args = '-arg=0'
+end
+
+class TestTool4 < ISS::Tool
+  self.bin  = 'fifa'
+  scope :servers, -> { 'type=server' }
+end
+
+assert 'ISS::Tool.bin' do
+  assert_equal 'testtool1', TestTool1.bin
+  assert_equal TestTool1.new.bin, TestTool1.bin
+end
+
+assert 'ISS::Tool.args' do
+  assert_equal '-arg=0', TestTool3.args
+  assert_equal TestTool3.new.args, TestTool3.args
+end
+
+assert 'ISS::Tool.path' do
+  assert_equal '/path/to', TestTool2.path
+  assert_equal '/path/to/testtool2', TestTool2.new.path
+  assert_equal 'testtool1', TestTool1.new.path
+end
+
+assert 'ISS::Tool.command' do
+  tool = TestTool3.new
+
+  assert_equal '/path/to/testtool3 -arg=0', tool.command
+  assert_equal '/path/to/testtool3 -arg=0', tool.command('-arg=1')
+  assert_equal '/path/to/testtool3 -opt=1 -arg=0', tool.command('-opt=1')
+  assert_equal '/path/to/testtool3 -h -arg=0', tool.command(args: '-h')
+  assert_equal '/path/to/testtool3 -arg=0 "a b"', tool.command(tail: '"a b"')
+end
+
+assert 'ISS::Tool#new' do
+  tool = TestTool1.new('-f=ski')
+
+  assert_equal 'testtool1 -f=ski', tool.command
+  assert_equal 'testtool1 -h -f=ski', tool.command('-h')
+
+  tool = TestTool1.new(args: '-f=ski', tail: '"a b"')
+
+  assert_equal 'testtool1 -f=ski "a b"', tool.command
+  assert_equal 'testtool1 -f=ski "a b" c', tool.command(tail: 'c')
+end
+
+assert 'ISS::Tool#scope' do
+  assert_equal 'fifa type=server', TestTool4.servers.command
+end
+
+assert 'ISS::Tool.call' do
+  tool = TestTool4.servers
+
+  def tool.exec(cmd)
+    cmd
+  end
+
+  assert_equal 'fifa type=server', tool.call
+end
